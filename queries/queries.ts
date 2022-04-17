f024589534a387d169d383852de522f810c60639
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-04-17 20:02:48 +0900

    feat: 🎸 必要なQuery, Typeの作成

---
+++ queries/queries.ts
@@ -0,0 +1,89 @@
+import { gql } from '@apollo/client'
+
+// ユーザーを新しい順に取得する (server side)
+export const GET_USERS = gql`
+  query GetUsers {
+    users(order_by: { created_at: asc }) {
+      name
+      id
+      created_at
+    }
+  }
+`
+
+// cacheからユーザーを新しい情報を取得する (client side)
+export const GET_USERS_LOCAL = gql`
+  query GetUsers {
+    users(order_by: { created_at: asc }) @client {
+      name
+      id
+      created_at
+    }
+  }
+`
+
+// ユーザーIDを新しい順に取得する
+export const GET_USERIDS = gql`
+  query GetUserIds {
+    users(order_by: { created_at: desc }) {
+      id
+    }
+  }
+`
+
+/**
+ * ユーザーIDでユーザーを取得する
+ * @param {uuid} id
+ */
+export const GET_USERBY_ID = gql`
+  query GetUserById($id: uuid!) {
+    users_by_pk(id: $id) {
+      id
+      name
+      created_at
+    }
+  }
+`
+
+/**
+ * 新しくユーザーを作成する
+ * @param {string} name
+ */
+export const CREATE_USER = gql`
+  mutation CreateUser($name: String!) {
+    insert_users_one(object: { name: $name }) {
+      created_at
+      id
+      name
+    }
+  }
+`
+
+/**
+ * ユーザーを更新する
+ * @param {uuid} id
+ * @param {string} name
+ */
+export const UPDATE_USER = gql`
+  mutation UpdateUser($id: uuid!, $name: String!) {
+    update_users_by_pk(pk_columns: { id: $id }, _set: { name: $name }) {
+      name
+      created_at
+      id
+    }
+  }
+`
+
+/**
+ * ユーザーを削除する
+ * @param {uuid} id ユーザーID
+ */
+export const DELETE_USER = gql`
+  mutation DeleteUser($id: uuid!) {
+    delete_users_by_pk(id: $id) {
+      id
+      name
+      created_at
+    }
+  }
+`
